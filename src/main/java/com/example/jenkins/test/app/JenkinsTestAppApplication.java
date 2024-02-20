c4a6906730b691edc310ce8687a35a8d6bc34bb9
deepanshu.k@ecomexpress.in <124904@EIHQTECH124904L.local>
2024-02-20 22:15:48 +0530

    initial commit

---
+++ src/main/java/com/example/jenkins/test/app/JenkinsTestAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkins.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Deepanshu");
+		SpringApplication.run(JenkinsTestAppApplication.class, args);
+	}
+
+}
